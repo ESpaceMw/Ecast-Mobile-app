cea673580993d00cc65ea0db35406b1466940d98
Rodger <rkumwanje@gmail.com>
2021-11-12 19:07:33 +0200

    s:sparkes: splash screen done

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,7 +1,27 @@
+import 'package:ecast/Utils/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void delay() async {
+    await Future.delayed(
+        const Duration(seconds: 3),
+        () => {
+              Navigator.pushReplacementNamed(context, '/wrapper'),
+            });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +30,20 @@ class SplashScreen extends StatelessWidget {
       body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        child: const Center(),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: const AssetImage('assets/logos/logo.png'),
+                width: MediaQuery.of(context).size.width * 0.8,
+              ),
+              const SpinKitThreeBounce(
+                color: kPrimaryColor,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:ecast/Screens/splash_screen.dart';
+import 'package:ecast/Utils/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      brightness: Brightness.dark,
+      scaffoldBackgroundColor: kBackgroundColor,
+      primaryColor: kPrimaryColor,
+    ),
     routes: {
       '/': (context) => const SplashScreen(),
     },
