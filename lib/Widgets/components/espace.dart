a59c0c455b112347a2ed15f1b1c75e6ff3772621
Rodger <rkumwanje@gmail.com>
2021-12-27 23:04:16 +0200

    components

---
+++ lib/Widgets/components/espace.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Espace extends StatefulWidget {
+  const Espace({Key? key}) : super(key: key);
+
+  @override
+  _EspaceState createState() => _EspaceState();
+}
+
+class _EspaceState extends State<Espace> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Widgets/components/plan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Plan extends StatefulWidget {
+  const Plan({Key? key}) : super(key: key);
+
+  @override
+  _PlanState createState() => _PlanState();
+}
+
+class _PlanState extends State<Plan> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
